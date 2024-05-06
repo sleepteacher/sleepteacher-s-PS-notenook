cbb6a01c7f4aced752d4ee46cf5f646655d9eafb
sleepteacher <cj455993@gmail.com>
2024-05-06 19:09:48 +0900

    new file:   .cph/.1647.cpp_d912178ee76087a853fbf9290a179f48.prob 	modified:   ".cph/.\353\217\204\354\213\234_\353\266\204\355\225\240_\352\263\204\355\232\215.cpp_6882db281045041622f946579ddb1177.prob" 	modified:   .vscode/launch.json 	modified:   .vscode/settings.json 	modified:   .vscode/tasks.json 	modified:   1647.cpp 	new file:   build/$main.exe 	modified:   build/1647.exe 	deleted:    build/Debug/1647.o 	modified:   build/Debug/outDebug.exe 	modified:   "build/Debug/\353\217\204\354\213\234_\353\266\204\355\225\240_\352\263\204\355\232\215.o" 	new file:   "build/\353\217\204\354\213\234_\353\266\204\355\225\240_\352\263\204\355\232\215.exe" 	new file:   "\353\217\204\354\213\234_\353\266\204\355\225\240_\352\263\204\355\232\215.cpp"

---
+++ 도시_분할_계획.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int UF[100001];
+int UF_find(int a) {
+    if (UF[a] != a) {
+        return UF[a] = UF_find(UF[a]);
+    }
+    return a;
+}
+
+int main() {
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ios::sync_with_stdio(false);
+
+    vector<vector<pair<int, int>>> G(1001);
+    int i, j, k, l, N, M, a, b, c, last = 0,result = 0;
+    cin >> N >> M;
+    for (i = 1; i <= N; i++) {
+        cin >> a ;
+        UF[i] = a;
+    }
+    for (i = 0; i < M; i++) {
+        
+        G[c].push_back({a, b});
+        
+    }
+
+    return 0;
+}
