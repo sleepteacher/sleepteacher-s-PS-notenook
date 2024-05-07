350c2747ecbe66e596575a7abe0f4447aed85144
sleepteacher <cj455993@gmail.com>
2024-05-07 15:00:55 +0900

    modified:   ".cph/.\353\217\204\354\213\234_\353\266\204\355\225\240_\352\263\204\355\232\215.cpp_6882db281045041622f946579ddb1177.prob" 	modified:   build/$main.exe 	modified:   "\353\217\204\354\213\234_\353\266\204\355\225\240_\352\263\204\355\232\215.cpp"

--- 도시_분할_계획.cpp
+++ 도시_분할_계획.cpp
@@ -16,18 +16,25 @@ int main() {
     ios::sync_with_stdio(false);
 
     vector<vector<pair<int, int>>> G(1001);
-    int i, j, k, l, N, M, a, b, c, last = 0,result = 0;
+    int i, j, k, l, N, M, a, b, c, last = 0, result = 0;
     cin >> N >> M;
     for (i = 1; i <= N; i++) {
-        cin >> a ;
-        UF[i] = a;
+        UF[i] = i;
     }
     for (i = 0; i < M; i++) {
-        
+        cin >> a >> b >> c;
         G[c].push_back({a, b});
-        
     }
-    
+    for (i = 1; i < 1001; i++) {
+        for (j = 0; j < G[i].size(); j++) {
+            if (UF_find(G[i][j].first) != UF_find(G[i][j].second)) {
+                last = i;
+                result += i;
+                UF[UF_find(G[i][j].first)] = UF_find(G[i][j].second);
+            }
+        }
+    }
+    cout << result - last << "\n";
 
     return 0;
 }
