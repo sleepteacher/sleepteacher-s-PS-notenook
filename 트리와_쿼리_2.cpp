107592f0729009954db32181bc36937bcf87ce96
sleepteacher <cj455993@gmail.com>
2024-05-13 06:47:51 +0900

    modified:   ".cph/.\355\212\270\353\246\254\354\231\200_\354\277\274\353\246\254_2.cpp_223df909dde4b6aefe5d90d8e300445a.prob" 	deleted:    LCA.cpp 	deleted:    LCS_2.cpp 	modified:   build/$main.exe 	modified:   "\355\212\270\353\246\254\354\231\200_\354\277\274\353\246\254_2.cpp"

--- 트리와_쿼리_2.cpp
+++ 트리와_쿼리_2.cpp
@@ -1,35 +1,71 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-vector<int> info[100001];
-vector<pair<int, int>> dp[100001][17];
+vector<pair<int, int>> info[100001];
+pair<int, long long> dp[100001][17];
 queue<int> Q;
-int , depth[100001];
+int depth[100001];
 bool visited[100001];
-int LCA(int a, int b) {
-    int i, j, k, l, dif, iter;
+
+int LCA(int a, int b, int c, int mode) {
+    int i, j, k, l, dif, iter, u = a, v = b;
     if (depth[a] < depth[b]) {
         i = a;
         a = b;
         b = i;
     }
+
     dif = depth[a] - depth[b], iter;
     for (i = 0; i < 17; i++) {
         iter = 1 << i;
         if ((dif | iter) == dif) {
-            a = dp[a][i];
+            a = dp[a][i].first;
         }
     }
     if (a != b) {
         for (i = 16; i >= 0; i--) {
-            if ((dp[a][i] != 0) && (dp[a][i] != dp[b][i])) {
-                a = dp[a][i];
-                b = dp[b][i];
+            if ((dp[a][i].first != 0) && (dp[a][i].first != dp[b][i].first)) {
+                a = dp[a][i].first;
+                b = dp[b][i].first;
             }
         }
-        a = dp[a][0];
+        a = dp[a][0].first;
     }
-    return a;
+    if (mode - 1) {
+        /*
+        2 - u to v로 가는 경로에 존재하는 정점에서 k번째 정점 출력
+        */
+        if (c < depth[a] - depth[u]) {
+            dif = c;
+            for (i = 0; i < 17; i++) {
+                iter = 1 << i;
+                if ((dif | iter) == dif) {
+                    u = dp[u][i].first;
+                }
+            }
+            return u;
+        } else if (c == depth[a] - depth[u]) {
+            return a;
+        } else {
+            dif = depth[v] -(c- (depth[a] - depth[u]));
+            /*
+            u = depth_2 , v = depth_3 이라고 할 때
+            구간 [1,4] 에서 탐색이 허용된다.
+            depth[a] + 1
+            */
+
+            cout << depth[v] << " " << c << " " << depth[a] << " " << depth[u]
+                 << "\n";
+            for (i = 0; i < 17; i++) {
+                iter = 1 << i;
+                if ((dif | iter) == dif) {
+                    v = dp[v][i].first;
+                }
+            }
+            return v;
+        }
+    } else {
+    }
 }
 
 int main() {
@@ -37,30 +73,36 @@ int main() {
     cout.tie(NULL);
     ios::sync_with_stdio(false);
 
-    int i, j, k, l, T, N, a, b;
+    int i, j, k, l, T, N, a, b, c, mode;
 
     fill(visited, visited + 100001, 1);
     cin >> N;
-    info[1].push_back(0);
-    dp[1][0] = 0, depth[1] = 1, visited[0] = 0;
-    for (i = 0; i < N - 1; i++) {
-        cin >> a >> b;
-        info[a].push_back(b);
-        info[b].push_back(a);
+    cin >> a >> b >> c;
+    dp[a][0].first = 0, depth[a] = 1, visited[0] = 0;
+    info[a].push_back({b, c});
+    info[b].push_back({a, c});
+    Q.push(a);
+    for (i = 0; i < N - 2; i++) {
+        cin >> a >> b >> c;
+        info[a].push_back({b, c});
+        info[b].push_back({a, c});
     }
-    Q.push(1);
+    
     while (!Q.empty()) {
         int vertex = Q.front();
         Q.pop();
         visited[vertex] = 0;
         for (i = 0; i < info[vertex].size(); i++) {
-            int now = info[vertex][i];
+            int now = info[vertex][i].first, val = info[vertex][i].second;
             if (visited[now]) {
                 depth[now] = depth[vertex] + 1;
-                dp[now][0] = vertex;
+                dp[now][0].first = vertex;
+                dp[now][0].second = val;
                 for (j = 0; j < 16; j++) {
-                    if (dp[dp[now][j]][j] != 0) {
-                        dp[now][j + 1] = dp[dp[now][j]][j];
+                    if (dp[dp[now][j].first][j].first != 0) {
+                        dp[now][j + 1].first = dp[dp[now][j].first][j].first;
+                        dp[now][j + 1].second =
+                            dp[dp[now][j].first][j].second + dp[now][j].second;
                     }
                 }
                 Q.push(now);
@@ -68,10 +110,16 @@ int main() {
         }
     }
     cin >> T;
+    cout <<depth[4] <<"\n";
     for (i = 0; i < T; i++) {
-        cin >> a >> b;
-        cout << LCA(a, b) << "\n";
-        // LCA(a, b);
+        cin >> mode;
+        if (mode - 1) {
+            cin >> a >> b >> c;
+            cout << LCA(a, b, c, mode) << "\n";
+        } else {
+            cin >> a >> b;
+            cout << LCA(a, b, 0, mode) << "\n";
+        }
     }
 
     return 0;
