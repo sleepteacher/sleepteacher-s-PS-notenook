791b6baba23d68f1549bae49128b4d789718381a
sleepteacher <cj455993@gmail.com>
2024-05-13 09:58:06 +0900

    modified:   "\353\262\204\353\270\224_\354\206\214\355\212\270.cpp"

---
+++ 버블_소트.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ios::sync_with_stdio(false);
+    int i, j, k, l, N;
+    cin >> N;
+    
+    for (i = 0; i < N; i++) {
+    }
+
+    return 0;
+}
