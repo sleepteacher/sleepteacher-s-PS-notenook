f8a417e7924e2f39c4fe65f36718ba19767a7d27
sleepteacher <cj455993@gmail.com>
2024-05-14 12:06:40 +0900

    modified:   ".cph/.\353\262\204\353\270\224_\354\206\214\355\212\270.cpp_2501b846bb0a8c30cda3598d94b6caf8.prob" 	modified:   .vscode/settings.json 	modified:   build/$main.exe 	modified:   "\353\262\204\353\270\224_\354\206\214\355\212\270.cpp"

--- 버블_소트.cpp
+++ 버블_소트.cpp
@@ -6,11 +6,25 @@ int main() {
     cin.tie(NULL);
     cout.tie(NULL);
     ios::sync_with_stdio(false);
-    int i, j, k, l, N;
+    int i, j, k, l, N, result = 1;
     cin >> N;
-    
+    vector<int> arr1(N), arr2(N), visited(1000001, 0);
+
     for (i = 0; i < N; i++) {
+        cin >> arr1[i];
+        arr2[i] = arr1[i];
     }
+    sort(arr1.begin(), arr1.end());
+    for (i = N - 1; i >= 0; i--) {
+        visited[arr2[i]]++;
+        j = upper_bound(arr1.begin(), arr1.end(), arr2[i]) - arr1.begin() -
+            visited[arr2[i]];
+        k = abs(j - i) + 1;
+        j = N - j;
+        result = max(min({j, k}), result);
+    }
+
+    cout << result;
 
     return 0;
 }
