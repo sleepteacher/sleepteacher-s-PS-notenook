799509fa04f9276666170f50ffe8deffd146c71b
sleepteacher <cj455993@gmail.com>
2024-06-18 17:33:08 +0900

    modified:   .cph/.ACM_Craft.cpp_ad64468624d37a5cff12df2e8135195a.prob 	new file:   ".cph/.\354\212\244\353\217\204\354\277\240.cpp_fe74bb973f263961c06be305716b3c5f.prob" 	modified:   .vscode/c_cpp_properties.json 	modified:   .vscode/launch.json 	modified:   ACM_Craft.cpp 	modified:   build/$main.exe 	new file:   "\354\212\244\353\217\204\354\277\240.cpp"

---
+++ 스도쿠.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+
+using namespace std;
+
+int main(){
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ios::sync_with_stdio(false);
+    string arr[9];
+    int i,j,k,l;
+    
+    return 0;
+}
