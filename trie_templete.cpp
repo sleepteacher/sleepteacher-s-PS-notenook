cb59004a86278ec08c8fb653f70af9e0f4492b0d
sleepteacher <cj455993@gmail.com>
2024-08-10 07:13:39 +0900

    new file:   ".cph/.\354\240\204\355\231\224\353\262\210\355\230\270_\353\252\251\353\241\235.cpp_edd0d58de5a21c74f876c4599e0038ee.prob" 	new file:   trie_templete.cpp 	new file:   "\354\240\204\355\231\224\353\262\210\355\230\270_\353\252\251\353\241\235.cpp"

---
+++ trie_templete.cpp
@@ -0,0 +1,68 @@
+#include <bits/stdc++.h>
+
+#define ARR_SIZE 256
+
+using namespace std;
+
+int con;
+
+int get_index(char a) {
+	// 인덱스 불러오기,
+	// mod 1 대문자 0 소문자
+	return a;
+}
+
+struct Trie {
+	bool is_string_end;
+	Trie *children[ARR_SIZE];
+	// 생성자
+	Trie() : is_string_end(0) {
+		memset(children, 0, sizeof(children));
+	}
+	// 소멸자
+	~Trie() {
+		for (int i = 0; i < ARR_SIZE; i++) {
+			if (children[i]) delete children[i];
+		}
+	}
+	void insert(const char *str) {
+		if (*str == 0) {
+			is_string_end = 1;
+		} else {
+			int next_index = get_index(*str);
+			if (is_string_end) {
+				con = 1;
+				return;
+			}
+
+			if (children[next_index] == 0) children[next_index] = new Trie;
+			children[next_index]->insert(str + 1);
+		}
+	}
+	Trie *find(const char *str) {
+		if (*str == 0) {
+			return this;
+		}
+		int next_index = get_index(*str);
+		if (children[next_index] == 0) return NULL;
+		return children[next_index]->find(str + 1);
+	}
+	bool string_has(const char *str) {
+		if (*str == 0 && is_string_end) {
+			return 1;
+		}
+		int next_index = get_index(*str);
+		if (children[next_index] == 0) return 0;
+		return children[next_index]->string_has(str + 1);
+	}
+};
+
+int main() {
+	cin.tie(NULL);
+	cout.tie(NULL);
+	ios::sync_with_stdio(false);
+	int TC;
+	cin >> TC;
+
+	return 0;
+}
